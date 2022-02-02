165a547ec5d789dc04b131f3653816e5b80447ee
latot <felipematas@yahoo.com>
2022-02-02 17:47:34 -0300

    minor fixes

--- shp2postgis.py
+++ shp2postgis.py
@@ -7,11 +7,21 @@ import jsonschema
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
-def sql_url(user, passwd, url, db):
-     return "postgresql://{user}:{passwd}@{url}/{db}".format(user=user, passwd=passwd, url=url, db=db)
+debug = True
 
-def get_safe_engine(user, passwd, url, db):
-    engine = sqlalchemy.create_engine(sql_url(user, passwd, url, db))
+def replace_keys(shp_config, keys):
+    for option in shp_config:
+        tmp = shp_config[option]
+        for key in keys:
+            tmp = tmp.replace(key, keys[key])
+        shp_config[option] = tmp
+    return shp_config
+
+def sql_url(user, password, url, db):
+     return "postgresql://{user}:{password}@{url}/{db}".format(user=user, password=password, url=url, db=db)
+
+def get_safe_engine(user, password, url, db):
+    engine = sqlalchemy.create_engine(sql_url(user, password, url, db))
     try:
         connection = engine.connect()
         connection.close()
@@ -24,7 +34,7 @@ def valid_db_config(ifolder):
     config_schema = None
     with open(os.path.join(script_path, "schemas/db_schema/schema.json")) as schema:
         config_schema = json.load(schema)
-    for config_file in glob.glob(os.path.join(ifolder, "**/dbs.json")):
+    for config_file in glob.glob(os.path.join(ifolder, "**/dbs.json"), recursive=True):
         data = None
         with open(config_file) as json_file:
             data = json.load(json_file)
@@ -34,23 +44,25 @@ def valid_shp_config(ifolder):
     with open(os.path.join(script_path, "schemas/shp_config_schema/schema.json")) as schema:
         config_schema = json.load(schema)
     for i in glob.glob(os.path.join(ifolder, "**/*.shp"), recursive=True):
-        with open("{}.{}".format(os.path.splitext(i)[0], "config")) as file_shp_config:
+        if not os.path.exists("{}.{}".format(os.path.splitext(i)[0], "json")):
+            continue
+        with open("{}.{}".format(os.path.splitext(i)[0], "json")) as file_shp_config:
             shp_config = json.load(file_shp_config)
         jsonschema.validate(shp_config, config_schema)
 
 def get_config(ifolder):
     config = {}
-    for config_file in glob.glob(os.path.join(ifolder, "**/dbs.json")):
+    for config_file in glob.glob(os.path.join(ifolder, "**/dbs.json"), recursive=True):
         with open(config_file) as json_file:
             data = json.load(json_file)
         for db in data:
             if 'password' not in data[db]:
                 engine = False
                 while not engine:
-                    passwd = input()
-                    engine = get_safe_engine(data[db]['user'], passwd, data[db]['url'], data[db]['db'])
+                    password = input()
+                    engine = get_safe_engine(data[db]['user'], password, data[db]['url'], data[db]['db'])
             else:
-                engine = get_safe_engine(data[db]['user'], data[db]['passwd'], data[db]['url'], data[db]['db'])
+                engine = get_safe_engine(data[db]['user'], data[db]['password'], data[db]['url'], data[db]['db'])
                 if not engine:
                     print("In the config file: {}".format(config_file))
                     print("Could not connect to the db: {}".format(db))
@@ -63,10 +75,16 @@ def shp2postgis(ifolder):
     valid_shp_config(ifolder)
     config = get_config(ifolder)
     for i in glob.glob(os.path.join(ifolder, "**/*.shp"), recursive=True):
+        if not os.path.exists("{}.{}".format(os.path.splitext(i)[0], "json")):
+            continue
         shape = geopandas.read_file(i)
-        with open("{}.{}".format(os.path.splitext(i)[0], "config")) as file_shp_config:
+        with open("{}.{}".format(os.path.splitext(i)[0], "json")) as file_shp_config:
             shp_config = json.load(file_shp_config)
-        geopandas_params_keys = ["if_exist",
+        rep_keys = {}
+        rep_keys["{file_name}"] = os.path.basename(i)
+        rep_keys["{file_name_no_ext}"] = os.path.splitext(rep_keys["{file_name}"])[0]
+        shp_config = replace_keys(shp_config, rep_keys)
+        geopandas_params_keys = ["if_exists",
                                 "schema",
                                 "index",
                                 "index_label",
@@ -76,6 +94,10 @@ def shp2postgis(ifolder):
         for p in geopandas_params_keys:
             if p in shp_config:
                 geopandas_params[p] = shp_config[p]
+        if debug:
+            geopandas_params["if_exists"] = "replace"
+        print(shp_config)
+        print("importing to postgis")
         shape.to_postgis(shp_config["name"], config[shp_config["db"]], **geopandas_params)
 
 if __name__ == "__main__":
@@ -83,7 +105,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'import shape files massively to postgis')
     parser.add_argument('folder', help='input folder')
     args = parser.parse_args()
-    if os.path.isdir(parser.folder):
-        shp2postgis(parser.folder)
+    if os.path.isdir(args.folder):
+        shp2postgis(args.folder)
     else:
         raise NameError("No input folder")

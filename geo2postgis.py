cb6cc7c6acff361c8f7793fea23d923ae2292567
latot <felipematas@yahoo.com>
2022-03-02 12:33:39 -0300

    minor fixes

--- geo2postgis.py
+++ geo2postgis.py
@@ -14,17 +14,19 @@ config = None
 
 def replace_keys(shp_config, keys):
     for option in shp_config:
+        if not isinstance(shp_config[option], str):
+            continue
         tmp = shp_config[option]
         for key in keys:
             tmp = tmp.replace(key, keys[key])
         shp_config[option] = tmp
     return shp_config
 
-def sql_url(user, password, url, db):
-     return "postgresql://{user}:{password}@{url}/{db}".format(user=user, password=password, url=url, db=db)
+def sql_url(user, password, url, port, db):
+     return "postgresql://{user}:{password}@{url}:{port}/{db}".format(user=user, password=password, url=url, port=port, db=db)
 
-def get_safe_engine(user, password, url, db):
-    engine = sqlalchemy.create_engine(sql_url(user, password, url, db))
+def get_safe_engine(user, password, url, port, db):
+    engine = sqlalchemy.create_engine(sql_url(user, password, url, port, db))
     try:
         connection = engine.connect()
         connection.close()
@@ -49,13 +51,15 @@ def get_config(ifolder):
         with open(config_file) as json_file:
             data = json.load(json_file)
         for db in data:
+            if 'port' not in data[db]:
+                data[db]['port'] = 5432
             if 'password' not in data[db]:
                 engine = False
                 while not engine:
                     password = input()
-                    engine = get_safe_engine(data[db]['user'], password, data[db]['url'], data[db]['db'])
+                    engine = get_safe_engine(data[db]['user'], password, data[db]['url'], data[db]['port'], data[db]['db'])
             else:
-                engine = get_safe_engine(data[db]['user'], data[db]['password'], data[db]['url'], data[db]['db'])
+                engine = get_safe_engine(data[db]['user'], data[db]['password'], data[db]['url'], data[db]['port'], data[db]['db'])
                 if not engine:
                     print("In the config file: {}".format(config_file))
                     print("Could not connect to the db: {}".format(db))
@@ -87,8 +91,10 @@ def shp2postgis(shp_file_path):
     if debug:
         geopandas_params["if_exists"] = "replace"
     if debug:
+        print("shape config {}".format(shp_file_path))
         print(shp_config)
         print("importing to postgis")
+        print(geopandas_params)
     shape.to_postgis(shp_config["name"], config[shp_config["db"]], **geopandas_params)
 
 def fast_json(file):
@@ -127,6 +133,9 @@ def geo2postgis(ifolder):
     global config
     valid_db_config(ifolder)
     config = get_config(ifolder)
+    if debug:
+        print("global config")
+        print(config)
     load_by_extensions(ifolder)
 
 if __name__ == "__main__":
